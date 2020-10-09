b26a98346734277f94cd3851845a9253c687de62
ekavbush <ekavbush@mail.ru>
2020-10-09 11:47:17 +0300

    09-10-20 11-46 работающая карусель Популярного

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,77 +4,66 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.min.css';
 
 function Carousel() {
-    return <OwlCarousel
-        className="owl-theme"
-        items="3"
-        autoplay
-        loop
-    >
-        <div className="item">
-            <div className="site-blocks inner-page overlay"
-                 data-aos="fade" data-stellar-background-ratio="0.5" style={{backgroundImage: "url(../img/cake-feature/c-feature-1.jpg)"}}>
-                <div className="container">
-                    {/*<div className="row align-items-center justify-content-center">*/}
-                        <div className="col-md-6" data-aos="fade">
-                            <h1 className="font-secondary font-weight-bold text-uppercase">Заголовок 1</h1>
-                            <span className="caption d-block">
-                                <h4>$29</h4>
-//                              <h3>Strawberry Cupcakes</h3>
-                            </span>
-                        </div>
-                    {/*</div>*/}
+
+    return <div className="cake_feature_inner">
+        <div className="main_title">
+            <h2>Наши популярные торты</h2>
+            <h5> Seldolor sit amet consect etur</h5>
+        </div>
+        <OwlCarousel
+            className="owl-theme"
+            items="4"
+            autoplay
+            loop
+            dots
+        >
+            <div className="item">
+                <div className="cake_feature_item">
+                    <div className="cake_img">
+                        <img src="../img/cake-feature/c-feature-1.jpg" alt=""/>
+                    </div>
+                    <div className="cake_text">
+                        <h4>$29</h4>
+                        <h3>Strawberry Cupcakes</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="item">
-            <div className="site-blocks inner-page overlay"
-                 data-aos="fade" data-stellar-background-ratio="0.5" style={{backgroundImage: "url(../img/cake-feature/c-feature-2.jpg)"}}>
-                <div className="container">
-                    {/*<div className="row align-items-center justify-content-center">*/}
-                        <div className="col-md-7 text-center" data-aos="fade">
-                            <h1 className="font-secondary font-weight-bold text-uppercase">Strawberry Cupcakes</h1>
-                            <span className="caption d-block">
-                                <h4>$29</h4>
-//                              <h3></h3>
-                            </span>
-                        </div>
-                    {/*</div>*/}
+            <div className="item">
+                <div className="cake_feature_item">
+                    <div className="cake_img">
+                        <img src="../img/cake-feature/c-feature-2.jpg" alt=""/>
+                    </div>
+                    <div className="cake_text">
+                        <h4>$29</h4>
+                        <h3>Strawberry Cupcakes</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="item">
-            <div className="site-blocks inner-page overlay"
-                 data-aos="fade" data-stellar-background-ratio="0.5" style={{backgroundImage: "url(../img/cake-feature/c-feature-3.jpg)"}}>
-                <div className="container">
-                    {/*<div className="row align-items-center justify-content-center">*/}
-                        <div className="col-md-7 text-center" data-aos="fade">
-                            <h1 className="font-secondary font-weight-bold text-uppercase">Заголовок 2</h1>
-                            <span className="caption d-block">
-                                <h4>$29</h4>
-//                              <h3>Strawberry Cupcakes</h3>
-                            </span>
-                        </div>
-                    {/*</div>*/}
+            <div className="item">
+                <div className="cake_feature_item">
+                    <div className="cake_img">
+                        <img src="../img/cake-feature/c-feature-3.jpg" alt=""/>
+                    </div>
+                    <div className="cake_text">
+                        <h4>$29</h4>
+                        <h3>Strawberry Cupcakes</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="item">
-            <div className="site-blocks-cover inner-page overlay"
-                 data-aos="fade" data-stellar-background-ratio="0.5" style={{backgroundImage: "url(../img/cake-feature/c-feature-4.jpg)"}}>
-                <div className="container">
-                    {/*<div className="row align-items-center justify-content-center">*/}
-                        <div className="col-md-7 text-center" data-aos="fade">
-                            <h1 className="font-secondary font-weight-bold text-uppercase">Заголовок 2</h1>
-                            <span className="caption d-block">
-                                <h4>$29</h4>
-//                              <h3>Strawberry Cupcakes</h3>
-                            </span>
-                        </div>
-                    {/*</div>*/}
+            <div className="item">
+
+                <div className="cake_feature_item">
+                    <div className="cake_img">
+                        <img src="../img/cake-feature/c-feature-4.jpg" alt=""/>
+                    </div>
+                    <div className="cake_text">
+                        <h4>$29</h4>
+                        <h3>Strawberry Cupcakes</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-    </OwlCarousel>
+        </OwlCarousel>
+    </div>
 }
 
 export default Carousel;
